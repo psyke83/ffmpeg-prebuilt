4b58cb576c4cc8cd8519bc9ec34ff53f7b357fe5
Conn O'Griofa <connogriofa@gmail.com>
2022-11-21 06:35:35 +0000

    Add patch to fix buffer delay on amfenc
    * Add patch that eliminates hw buffer and hardcoded sleep in amfenc when avctx->max_b_frames is 0
    * Update new and old patches to point to correct repository location

--- build/ffmpeg_extra.sh
+++ build/ffmpeg_extra.sh
@@ -1,6 +1,7 @@
 _pre_configure(){
     #
     # Apply a patch from ffmpeg's patchwork site.
-    do_patch "https://raw.githubusercontent.com/TheElixZammuto/sunshine-prebuilt/master/build/0001-patch-idr-on-amf.patch" patch
+    do_patch "https://raw.githubusercontent.com/LizardByte/ffmpeg-prebuilt/master/build/0001-patch-idr-on-amf.patch" patch
+    do_patch "https://raw.githubusercontent.com/LizardByte/ffmpeg-prebuilt/master/build/0002-disable-hwbuffers.patch" patch
     #
 }
